36daf140796b7d09e8f0b2c6b7adc728cf10b075
root <root@1357619-cj56605.tw1.ru>
2023-03-27 20:00:15 +0300

    mailer

--- index.js
+++ index.js
@@ -7,10 +7,8 @@ import Router from "./routes/router.js";
 import cors from "cors";
 import https from "https";
 import path from "path";
-import fs from "fs";
 import AdminRouter from "./routes/Admin/router.js";
 import { fileURLToPath } from "url";
-
 // Dirname on ES6 Module
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -18,7 +16,6 @@ const __dirname = path.dirname(__filename);
 //configs
 const port = config.get("server.port");
 const bd = config.get("databse.mongoURL");
-const isDevelopment = config.get("dev");
 
 //routing
 const app = express();
@@ -37,9 +34,9 @@ app.use(function (req, res, next) {
 app.use(express.json());
 
 // ROUTES
-
 app.get("/", (req, res) => {
-  return res.status(200);
+  console.log('kpela')
+  return res.status(200).send({connected:true});
 });
 app.use("/user", UserRouter);
 app.use("/api", Router);
--- utils/mail.js
+++ utils/mail.js
@@ -1,24 +1,23 @@
 import nodemailer from "nodemailer";
 import config from "config";
-import mg from 'nodemailer-mailgun-transport'
-const key = config.get("nodemailer.api_key");
-const domain = config.get("nodemailer.maligu_domain");
-const auth = {
-  auth: {
-    api_key: key,
-    domain: domain,
-  }
-}
 
-const nodemailerMailgun = nodemailer.createTransport(mg(auth));
+const user = config.get('nodemailer.user')
+const pass = config.get('nodemailer.pass')
 
 export async function mailGenerator(from, to, inputs) {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    auth:  {
+      user, // Sender mail
+      pass // Sender mail password
+  },
+  });
   let mailOptions = {
     from: from,
     to: to,
     ...inputs,
   };
-  nodemailerMailgun.sendMail(mailOptions, function (error, info) {
+  transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
       console.log(error);
     } else {
